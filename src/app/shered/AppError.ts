074f35de70af402abfe16bf43bde85d1522311ad
morshed0099 <mdgolammorshed0099@gmail.com>
2024-03-26 13:17:55 +0600

    custome appError class define exted of Error class

---
+++ src/app/shered/AppError.ts
@@ -0,0 +1,12 @@
+class AppError extends Error {
+  statusCode: number;
+  constructor(statusCode: number, messgae: string | undefined, stack = "") {
+    super(messgae);
+    this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
